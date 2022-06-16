ee7fda8fa9644ef334d5ed3fa68a54a1ad625a7e
Dexter Ramos <blitzdex27@gmail.com>
2022-06-16 22:32:27 +0800

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+
+const u = process.env.U;
+const p = process.env.P;
+const ukey = process.argv[2];
+console.log(ukey);
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+  });
+  const page = await browser.newPage();
+  await page.goto("https://facebook.com/login");
+  await page.waitForNavigation({ waitUntil: "load" });
+  await page.type("#email", u);
+  await page.type("#pass", p);
+  await page.click("#loginbutton");
+}
+
+run();
